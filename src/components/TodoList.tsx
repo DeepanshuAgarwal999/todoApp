137501eac4ffb32afba3f0242768d56477a04fe5
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-01-13 10:00:57 +0530

    fix ui and omplemntation issues

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { useTodo } from '../context/TodoProvider'
-import { Check } from 'lucide-react';
-import Filter from './Filter';
+import React, { useEffect, useState } from 'react';
+import { useTodo } from '../context/TodoProvider';
+import CustomInputField from './CustomInputField';
 
 interface Todo {
     id: number;
@@ -10,80 +9,51 @@ interface Todo {
 }
 
 const TodoList = () => {
-    const { todos, removeTodo, editTodo, handleToggle, clearCompleted } = useTodo()
-    const [inputValue, setInputValue] = useState('')
-    const [isEdit, setIsEdit] = useState<number | null>(null)
-    const [filteredTodos, setFilteredTodos] = useState<TodoList>({})
+    const { todos, removeTodo, handleToggle } = useTodo();
+    const [todoToEdit, setTodoToEdit] = useState<number | null>(null);
 
-    useEffect(() => {
-        setFilteredTodos(todos)
-    }, [todos])
-    // Start editing on double-click
-    const handleDoubleClick = (id: number, title: string) => {
-        setIsEdit(id)
-        setInputValue(title)
-    }
-    // Save edited todo
-    const handleSave = (id: number) => {
-        if (!inputValue) alert('Field cannot be empty')
-        editTodo(id, inputValue)
-        setIsEdit(null)
-        setInputValue('')
-    }
-    console.log(filteredTodos);
+    const handleDoubleClick = (id: number) => {
+        setTodoToEdit(id);
+    };
 
     return (
-        <div>
-            {Object.values(filteredTodos).map((todo: Todo) => (
+        <div className="">
+            {Object.values(todos).map((todo: Todo) => (
                 <div
                     key={todo.id}
-                    className='mt-1 bg-white group border-b max-w-3xl mx-auto p-4 h-16 flex items-center justify-between gap-8 shadow-lg focus-within:ring-2 ring-red-700'
-
+                    className="flex items-center group w-full bg-[#FEFEFE] border "
                 >
-                    {isEdit === todo.id ? (
-                        <form action="" onSubmit={() => handleSave(todo.id)}>
-                            <input
-                                type="text"
-                                value={inputValue}
-                                onChange={(e) => setInputValue(e.target.value)}
-                                onBlur={() => handleSave(todo.id)}
-                                autoFocus
-                                className='w-full h-14 outline-none bg-transparent text-3xl pl-6'
-                            />
-                        </form>
+                    {todoToEdit === todo.id ? (
+                        <CustomInputField mode='edit' todo={todo} setTodoToEdit={setTodoToEdit} />
                     ) : (
                         <div
-                            className='flex items-center gap-4 w-full'
-                            onDoubleClick={() => handleDoubleClick(todo.id, todo.title)}
+                            className="flex items-center gap-6 w-full py-4 px-4"
+                            onDoubleClick={() => handleDoubleClick(todo.id)}
                         >
                             <input
                                 type="checkbox"
                                 checked={todo.active}
                                 onChange={() => handleToggle(todo.id)}
+
                             />
-                            <p className={`text-3xl truncate ${todo.active ? "line-through text-gray-400" : "underline"}`}>{todo.title}</p>
+                            <p className={`text-2xl truncate ${todo.active ? 'line-through text-gray-400' : ''}`}>
+                                {todo.title}
+                            </p>
                         </div>
                     )}
-
                     {
-                        isEdit === todo.id ? <button
-                            className='text-3xl text-green-600'
-                            onClick={() => handleSave(todo.id)}
+                        !todoToEdit && <button
+                            className={`text-xl active:border-red-600 active:border p-2 mr-4 text-gray-600 hover:text-red-600 hidden ease-in-out duration-75 group-hover:block font-bold font-sans ${todo.active ? 'text-gray-400' : ''
+                                }`}
+                            onClick={() => removeTodo(todo.id)}
                         >
-                            <Check />
+                            X
                         </button>
-                            : <button
-                                className={`text-xl text-red-600 ${todo.active ? "text-gray-400" : ""}`}
-                                onClick={() => removeTodo(todo.id)}
-                            >
-                                X
-                            </button>
                     }
                 </div>
             ))}
-            <Filter setFilteredTodos={setFilteredTodos} />
         </div>
-    )
-}
+    );
+};
 
-export default TodoList
+export default TodoList;
